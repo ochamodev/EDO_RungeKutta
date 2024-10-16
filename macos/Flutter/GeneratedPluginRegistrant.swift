73d505a37a2ad665aaabf32137eddf92f156d81a
ochamo <cheleyotto98@gmail.com>
2024-10-16 14:02:01 -0600

    chore: add generated files

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,10 @@
 import FlutterMacOS
 import Foundation
 
+import macos_window_utils
+import system_theme
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  MacOSWindowUtilsPlugin.register(with: registry.registrar(forPlugin: "MacOSWindowUtilsPlugin"))
+  SystemThemePlugin.register(with: registry.registrar(forPlugin: "SystemThemePlugin"))
 }
--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -3,6 +3,8 @@
 #
 
 list(APPEND FLUTTER_PLUGIN_LIST
+  flutter_acrylic
+  system_theme
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
