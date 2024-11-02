5b6e921e9957fe95e82eb1ec2c19d3885bfe69e6
chamodeveloper <99205618+chamodeveloper@users.noreply.github.com>
2024-11-02 01:58:11 -0600

    feat: Create separator widget

---
+++ lib/util/component/separator.dart
@@ -0,0 +1,14 @@
+
+import 'package:fluent_ui/fluent_ui.dart';
+
+class Separator extends StatelessWidget {
+
+  const Separator({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const SizedBox(
+      height: 20,
+    );
+  }
+}
